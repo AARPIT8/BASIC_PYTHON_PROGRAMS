66c207c5cf5be4f124b169cfe6cc5d657fd15045
ARPIT <arpitdmauryaa@gmail.com>
2025-12-29 12:17:19 +0530

    Add area and perimeter of circle
    This commit adds a python program to calculate area and perimeter of a circle

---
+++ area__&_perimeter_of_circle.py
@@ -0,0 +1,6 @@
+import math
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius ** radius
+perimeter = 2 * math.pi * radius
+print("Area of the circle:", area)
+print("Perimeter (Circumference) of the circle:", perimeter)
